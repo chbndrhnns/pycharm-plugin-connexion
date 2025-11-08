caf5bef9434143cae2f131989593398f9101b941
chbndrhnns <chbndrhnns@users.noreply.github.com>
2025-11-08 22:59:53 +0100

    test: Enable MockSdk

--- src/test/kotlin/com/github/chbndrhnns/intellijplatformplugincopy/intention/wrap/TestBase.kt
+++ src/test/kotlin/com/github/chbndrhnns/intellijplatformplugincopy/intention/wrap/TestBase.kt
@@ -1,11 +1,21 @@
 package com.github.chbndrhnns.intellijplatformplugincopy.intention.wrap
 
 import com.github.chbndrhnns.intellijplatformplugincopy.MyPlatformTestCase
+import com.intellij.openapi.projectRoots.ProjectJdkTable
+import com.intellij.openapi.util.Disposer
 import com.jetbrains.python.inspections.PyTypeCheckerInspection
+import com.jetbrains.python.psi.LanguageLevel
+import jetbrains.python.fixtures.PythonMockSdk
 
 abstract class TestBase : MyPlatformTestCase() {
     override fun setUp() {
         super.setUp()
+        val sdk = PythonMockSdk.create(LanguageLevel.PYTHON311, myFixture.tempDirFixture.getFile("/")!!)
+        Disposer.register(testRootDisposable) {
+            com.intellij.openapi.application.runWriteAction {
+                ProjectJdkTable.getInstance().removeJdk(sdk)
+            }
+        }
         myFixture.enableInspections(PyTypeCheckerInspection::class.java)
     }
 }
