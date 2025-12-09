fb080d27472dde4276603d2b223be01391e67f38
chbndrhnns <chbndrhnns@users.noreply.github.com>
2025-12-09 10:23:45 +0100

    fix: Check for python version on startup and when sdk changes

--- src/main/kotlin/com/github/chbndrhnns/intellijplatformplugincopy/startup/PythonVersionStartupActivity.kt
+++ src/main/kotlin/com/github/chbndrhnns/intellijplatformplugincopy/startup/PythonVersionStartupActivity.kt
@@ -4,10 +4,27 @@ import com.github.chbndrhnns.intellijplatformplugincopy.python.PythonVersionGuar
 import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootEvent
+import com.intellij.openapi.roots.ModuleRootListener
+import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.startup.ProjectActivity
 
 class PythonVersionStartupActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
+        checkPythonVersion(project)
+
+        project.messageBus.connect(project).subscribe(ModuleRootListener.TOPIC, object : ModuleRootListener {
+            override fun rootsChanged(event: ModuleRootEvent) {
+                checkPythonVersion(project)
+            }
+        })
+    }
+
+    private fun checkPythonVersion(project: Project) {
+        if (ProjectRootManager.getInstance(project).projectSdk == null) {
+            return
+        }
+
         if (!PythonVersionGuard.isSatisfied(project)) {
             NotificationGroupManager.getInstance()
                 .getNotificationGroup("Python DDD Toolkit")
