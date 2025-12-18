7715fae2eb8701f109fda2181ae3efbf9c6a9579
chbndrhnns <chbndrhnns@users.noreply.github.com>
2025-12-18 14:09:04 +0100

    feat: Suppress rename to self and add "Add self"

---
+++ src/main/kotlin/com/github/chbndrhnns/intellijplatformplugincopy/intention/AddSelfParameterIntention.kt
@@ -0,0 +1,46 @@
+package com.github.chbndrhnns.intellijplatformplugincopy.intention
+
+import com.intellij.codeInsight.intention.PriorityAction
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.util.parentOfType
+import com.jetbrains.python.psi.PyElementGenerator
+import com.jetbrains.python.psi.PyFunction
+
+class AddSelfParameterIntention : PsiElementBaseIntentionAction(), PriorityAction {
+    override fun getFamilyName() = "Add 'self' parameter"
+    override fun getText() = "Add 'self' parameter"
+
+    override fun getPriority(): PriorityAction.Priority = PriorityAction.Priority.TOP
+
+    override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean {
+        val function = element.parentOfType<PyFunction>() ?: return false
+        val decorators = function.decoratorList?.decorators ?: emptyArray()
+        if (decorators.any { it.name == "staticmethod" }) return false
+
+        val parameters = function.parameterList.parameters
+        if (parameters.isEmpty()) return true
+
+        val firstParam = parameters[0]
+        return firstParam.name != "self"
+    }
+
+    override fun invoke(project: Project, editor: Editor?, element: PsiElement) {
+        val function = element.parentOfType<PyFunction>() ?: return
+        val params = function.parameterList
+
+        val generator = PyElementGenerator.getInstance(project)
+        val selfParam = generator.createParameter("self")
+
+        val firstParam = params.parameters.firstOrNull()
+        if (firstParam != null) {
+            params.addBefore(selfParam, firstParam)
+            val comma = generator.createComma().psi
+            params.addBefore(comma, firstParam)
+        } else {
+            params.addParameter(selfParam)
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/chbndrhnns/intellijplatformplugincopy/intention/suppressor/RenameToSelfFilter.kt
@@ -0,0 +1,20 @@
+package com.github.chbndrhnns.intellijplatformplugincopy.intention.suppressor
+
+import com.intellij.codeInsight.daemon.impl.IntentionActionFilter
+import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.openapi.project.DumbAware
+import com.intellij.psi.PsiFile
+import com.jetbrains.python.psi.PyFile
+
+class RenameToSelfFilter : IntentionActionFilter, DumbAware {
+    override fun accept(intentionAction: IntentionAction, file: PsiFile?): Boolean {
+        if (file !is PyFile) return true
+
+        val text = intentionAction.text
+        if (text != "Rename to 'self'" && text != "Rename to self") {
+            return true
+        }
+
+        return false
+    }
+}
